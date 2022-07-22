fba0dacb3868ea7a576e399fdab992cfa202b76c
dev4ult <nibrasbiasaaja@gmail.com>
2022-07-22 22:38:22 +0700

    final

--- js/intersectionObserver.js
+++ js/intersectionObserver.js
@@ -1,6 +1,9 @@
 const fadeRight = document.querySelectorAll('.fade-right');
 const fadeLeft = document.querySelectorAll('.fade-left');
 
+const header = document.querySelector('header');
+const arrowUp = document.querySelector('.arrow-up');
+
 const appearSelector = {
   treshold: 1,
 };
@@ -15,6 +18,27 @@ const appearOnScroll = new IntersectionObserver(function (entries, appearOnScrol
     }
   });
 }, appearSelector);
+
+const headerSelector = {
+  treshold: 1,
+  rootMargin: '0px 0px -250px 0px',
+};
+
+const hideOnHeader = new IntersectionObserver(function (entries, hideOnHeader) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      arrowUp.classList.remove('hidden');
+      arrowUp.classList.add('block');
+      console.log('is not intersecting');
+    } else {
+      arrowUp.classList.add('hidden');
+      arrowUp.classList.remove('block');
+      console.log('is intersecting');
+    }
+  });
+}, headerSelector);
+
+hideOnHeader.observe(header);
 
 fadeRight.forEach((item) => {
   appearOnScroll.observe(item);
